d47db88eccf55b76278a0a7458a1f35ad0238556
ngmtuandev <nguyenmanhtuancomputer@gmail.com>
2023-12-18 17:08:48 +0700

    'auth-verify'

--- frontend/src/pages/client/Home.tsx
+++ frontend/src/pages/client/Home.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Banner } from "../../components";
+import { useUserStore } from "../../store/useUserStore";
 
 const Home = () => {
+  const { getUserCurrent, token, user_current } = useUserStore();
+  console.log("user_current ????????????", user_current);
+  useEffect(() => {
+    getUserCurrent();
+  }, [token]);
   return (
     <div className="">
       <div>
